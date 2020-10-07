6f5bb138dc8f33d56a4d57316d2de8a18937fc54
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-10-07 14:50:42 -0600

    notes of rust commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("We can build a project using \"cargo build\".");
+    println!("We can build and run a project in one step using \"cargo build\".");
+    println!("We can build a project without producing a binary to check for errors using \"cargo check\".");
+    println!("Instead of saving the result of build in the same directory as our code, Cargo stores it in the target/debug directory.");
 }
